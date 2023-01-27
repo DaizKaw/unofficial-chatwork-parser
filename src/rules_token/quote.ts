e9b55c2cf6a661f6f25c55661178c1021b11a9df
DaizKaw <123338017+DaizKaw@users.noreply.github.com>
2023-01-27 01:53:40 +0000

    fix: rules_token qtOpen

--- src/rules_token/quote.ts
+++ src/rules_token/quote.ts
@@ -1,48 +1,84 @@
 import { ITokenizer, TokenRule } from "../types/token"
-const re =
-  /^((\[qt\]\[qtmeta aid=(\d+) time=(\d+)\])|([引用 aid=(\d+) time=(\d+)]))/
+const re1 = /^\[qt\]\[qtmeta aid=(\d+) time=(\d+)\]/
+const re2 = /^\[引用 aid=(\d+) time=(\d+)\]/
 export const qtOpen: TokenRule = (
   len: number,
   src: string,
   t: ITokenizer,
   silent: Boolean
 ): Boolean => {
-  let m: RegExpMatchArray | null
-  if ((m = src.slice(t.pos, t.pos + 50).match(re)) === null) {
+  const m1: RegExpMatchArray | null = src.slice(t.pos, t.pos + 50).match(re1)
+  const m2: RegExpMatchArray | null = src.slice(t.pos, t.pos + 50).match(re2)
+  if (!m1 && !m2) {
     return false
   }
   if (silent) {
     return true
   }
-  let pos = t.pos
-  t.tokens.push({
-    type: "quote_open",
-    value: "",
-    position: {
-      start: pos,
-      end: pos + 11,
-    },
-  })
-  pos += 11
-  t.tokens.push({
-    type: "text",
-    value: m[1],
-    position: {
-      start: pos,
-      end: pos + 5 + m[1].length,
-    },
-  })
-  pos += 5 + m[1].length
-  t.tokens.push({
-    type: "text",
-    value: m[2],
-    position: {
-      start: pos,
-      end: pos + 7 + m[2].length,
-    },
-  })
-  t.pos += m[0].length
-  return true
+
+  if (m1) {
+    let pos = t.pos
+    t.tokens.push({
+      type: "quote_open",
+      value: "",
+      position: {
+        start: pos,
+        end: pos + 11,
+      },
+    })
+    pos += 11
+    t.tokens.push({
+      type: "text",
+      value: m1[1],
+      position: {
+        start: pos,
+        end: pos + 5 + m1[1].length,
+      },
+    })
+    pos += 5 + m1[1].length
+    t.tokens.push({
+      type: "text",
+      value: m1[2],
+      position: {
+        start: pos,
+        end: pos + 7 + m1[2].length,
+      },
+    })
+    t.pos += m1[0].length
+    return true
+  } else if (m2) {
+    let pos = t.pos
+    t.tokens.push({
+      type: "quote_open",
+      value: "",
+      position: {
+        start: pos,
+        end: pos + 3,
+      },
+    })
+    pos += 3
+    t.tokens.push({
+      type: "text",
+      value: m2[1],
+      position: {
+        start: pos,
+        end: pos + 5 + m2[1].length,
+      },
+    })
+    pos += 5 + m2[1].length
+    t.tokens.push({
+      type: "text",
+      value: m2[2],
+      position: {
+        start: pos,
+        end: pos + 7 + m2[2].length,
+      },
+    })
+    t.pos += m2[0].length
+    return true
+  } else {
+    return false
+  }
 }
 
 export const qtClose: TokenRule = (
@@ -53,8 +89,8 @@ export const qtClose: TokenRule = (
 ): Boolean => {
   if (
     len - t.pos < 5 ||
-    src.slice(t.pos, t.pos + 5) !== "[/qt]" ||
-    src.slice(t.pos, t.pos + 5) !== "[/引用]"
+    (src.slice(t.pos, t.pos + 5) !== "[/qt]" &&
+      src.slice(t.pos, t.pos + 5) !== "[/引用]")
   ) {
     return false
   }
